987b20bdcc0e4c156a8148047b853bf53c728b75
Andreea Alina <andreeacaragea95@gmail.com>
2025-11-10 17:20:30 +0200

    Change login page file type and structure

---
+++ project/viewer/login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BookVerse | Login</title>
+
+  <style>
+    body, html
+    {
+      /* This makes the header and the footer stick to top and bottom correctly */
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      /* This makes the space in between footer and header to be high enough to cover the entire page*/
+      height: 100%;
+    }
+
+    main
+    {
+      flex: 1; /*Makes main to take up all the space left between footer and header */
+      background-color: #fff6deff;
+    }
+
+  </style>
+</head>
+
+<body>
+  <?php include('header.php'); ?>
+
+  <main>
+  </main>
+
+  <?php include('footer.php'); ?>
+</body>
+
+</html>
